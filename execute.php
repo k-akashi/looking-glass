f2387e031368899aa8c5142623df6db93252082b
Kunio AKASHI <k-akashi@interop-tokyo.net>
2023-06-13 00:46:40 +0900

    update

--- execute.php
+++ execute.php
@@ -48,11 +48,11 @@ if (isset($_POST['query']) && !empty($_POST['query']) &&
   $hostname = trim($_POST['routers']);
   $parameter = trim(htmlspecialchars($_POST['parameter']));
 
-  if (filter_var(($parameter, FILTER_VALIDATE_IP) == False) {
-      $error = "Invalid IP address.";
-      print(json_encode(array('error' => $error)));
-      return;
-  }
+  //if (filter_var(($parameter, FILTER_VALIDATE_IP) == False) {
+  //    $error = "Invalid IP address.";
+  //    print(json_encode(array('error' => $error)));
+  //    return;
+  //}
 
   // Check if query is disabled
   if (!isset($config['doc'][$query]['command'])) {
